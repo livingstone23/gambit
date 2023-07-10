4923987355ccb1cbcb538510a7a2443f7496040b
livingstone23 <livingstone23@gmail.com>
2023-07-10 18:52:45 +0200

    Ultimo Commit

--- bd/order.go
+++ bd/order.go
@@ -38,7 +38,7 @@ func InsertOrder(o models.Orders) (int64, error) {
 	}
 
 	for _, od := range o.OrdersDetails {
-		sentencia = "INSERT INTO orders_detail (OD_OrderId, OD_ProdId, OD_ProdId, OD_Quantity, OD_Price) VALUES (" + strconv.Itoa(int(LastInsertId))
+		sentencia = "INSERT INTO orders_detail (OD_OrderId, OD_ProdId,  OD_Quantity, OD_Price) VALUES (" + strconv.Itoa(int(LastInsertId))
 		sentencia += "," + strconv.Itoa(od.OD_ProdId) + "," + strconv.Itoa(od.OD_Quantity) + "," + strconv.FormatFloat(float64(od.OD_Price), 'f', -1, 64) + ")"
 		fmt.Println(sentencia)
 
